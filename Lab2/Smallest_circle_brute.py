150d81f2164ee0ad7f62e83068b402a0cc9209b6
adsamaz <33390767+adsamaz@users.noreply.github.com>
2018-11-28 12:16:29 +0100

    some scuffierno

--- Lab2/Smallest_circle_brute.py
+++ Lab2/Smallest_circle_brute.py
@@ -3,9 +3,9 @@ import math
 
 def smallest_circle_brute(p):
     c_min = Circle2P((-math.inf, -math.inf), (math.inf, math.inf))
-
     for a in p:
         for b in p:
+            valid = True
             if b == a:
                 continue
             circle = Circle2P(a, b)
@@ -13,8 +13,10 @@ def smallest_circle_brute(p):
                 if d == a or d == b:
                     continue
                 if not inside_circle_2p(circle, d):
+                    valid = False
                     break
-            if circle.diameter < c_min.diameter:
+            print("Circle: " + str(circle.midpoint) + str(circle.radius))
+            if valid and circle.diameter < c_min.diameter:
                 c_min = circle
 
     for a in p:
@@ -35,6 +37,14 @@ def smallest_circle_brute(p):
     return c_min
 
 
+def mid_point(p1, p2):
+    return (p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2     # tuple
+
+
+def distance(p1, p2):
+    return math.hypot(p1[0] - p2[0], p1[1] - p2[1])
+
+
 def inside_circle_2p(circle, d):
     distance = math.sqrt((circle.midpoint[0] - d[0])**2 + (circle.midpoint[1] - d[1])**2)
     if distance <= circle.radius:
@@ -42,8 +52,16 @@ def inside_circle_2p(circle, d):
     else:
         return False
 
+def sort_clockwise(p1, p2, p3):
+    p = [p1, p2, p3]
+    p.sort()
+    #[0], p1[1], p2[0], p2[1], p3[0], p3[1]
+    #if p[0][1] > p[1][1]:
+    #    if p[1][1] <
+
 
 def inside_circle_3p(circle, d):
+    #sort_clockwise(circle.p1, circle.p2, circle.p3)
     adx = circle.p1[0] - d[0]
     ady = circle.p1[1] - d[1]
     bdx = circle.p2[0] - d[0]
@@ -64,13 +82,14 @@ def inside_circle_3p(circle, d):
     else:
         return False
 
+
 class Circle2P(object):
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-        self.diameter = math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
+        self.diameter = distance(p1, p2)
         self.radius = self.diameter / 2
-        self.midpoint = ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)  # tuple
+        self.midpoint = mid_point(p1, p2)
 
 
 class Circle3P(object):
@@ -78,9 +97,11 @@ class Circle3P(object):
         self.p1 = p1
         self.p2 = p2
         self.p3 = p3
+        self.diameter = math.inf
+        self.radius = math.inf
         x1, y1, x2, y2, x3, y3 = p1[0], p1[1], p2[0], p2[1], p3[0], p3[1]
 
-        A = x2 - x1
+        """A = x2 - x1
         B = y2 - y1
         C = x3 - x1
         D = y3 - y1
@@ -90,22 +111,24 @@ class Circle3P(object):
         if G == 0:
             self.radius = 0
         x = (D * E - B * F) / G
-        y = (A * F - C * E) / G
-        """
-        ma = (y2-y1)/(x2-x1)
-        mb = (y3-y2)/(x3-x2)
-        x = (ma*mb*(y1-y3) + mb*(x1+x2) - ma*(x2+x3))/(2*(mb-ma))
-        if not ma == 0:
-            y = (-1/ma)*(x-(x1+x2)/2)+(y1+y2)/2
-        else:
-            y = (-1/mb)*(x-(x2+x3)/2)+(y2+y3)/2
-        """
+        y = (A * F - C * E) / G"""
+        try:
+            ma = (y2-y1)/(x2-x1)
+            mb = (y3-y2)/(x3-x2)
+            x = (ma*mb*(y1-y3) + mb*(x1+x2) - ma*(x2+x3))/(2*(mb-ma))
+            if not ma == 0:
+                y = (-1/ma)*(x-(x1+x2)/2)+(y1+y2)/2
+            else:
+                y = (-1/mb)*(x-(x2+x3)/2)+(y2+y3)/2
 
-        self.radius = math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1))
-        self.diameter = self.radius * 2
+            self.radius = math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1))
+            self.diameter = self.radius * 2
+        except ZeroDivisionError:
+            print("No circle could be made")
 
 
-c = smallest_circle_brute([(25, 25), (50, 50), (25, 50), (50, 25)])
-
-print(c.diameter)
+c = smallest_circle_brute([(1, 1), (1, 2), (2, 2), (2, 1), (4, 1), (3, 7), (10, 10)])
+# c = Circle3P((1, 1), (2, 2), (1, 2))
+print(c)
+print(c.radius)
 print(c.midpoint)
