2cff025e8994bc97765b203e80cc36f3b9f62857
adsamaz <33390767+adsamaz@users.noreply.github.com>
2018-12-14 15:56:26 +0100

    1 of leathal

--- Lab3/Range_tree.py
+++ Lab3/Range_tree.py
@@ -20,35 +20,30 @@ class Node:
         self.number_of_points = 0
         self.max_weight = 0                     # Largest w in the subtree
 
-    def set_properties(self, number_of_points, max_weight):
-        self.number_of_points = number_of_points
-        self.max_weight = max_weight
-
 class Range_tree:
 
-    def __init__(self, points):
+    def __init__(self, input_points):
+        points = sorted(input_points)
         self.root = self.create_tree(points)
         self.add_properties(self.root)          # Traverse the tree and add number of points and max weight to all nodes
 
-
-    def create_tree(self, input_points):
-        points = sorted(input_points)
+    def create_tree(self, points):
         length = len(points)
         if length == 1:
             return Leaf(points.pop())
         else:
-            left_subtree = Range_tree(points[:length//2])
-            right_subtree = Range_tree(points[length//2:])
+            left_subtree = self.create_tree(points[:length//2])
+            right_subtree = self.create_tree(points[length//2:])
 
-            if isinstance(left_subtree.root, Node):
-                splitting_value = left_subtree.root.max_value
+            if isinstance(left_subtree, Node):
+                splitting_value = left_subtree.max_value
             else:
-                splitting_value = left_subtree.root.p
+                splitting_value = left_subtree.p
 
-            if isinstance(right_subtree.root, Node):
-                max_value = right_subtree.root.max_value
+            if isinstance(right_subtree, Node):
+                max_value = right_subtree.max_value
             else:
-                max_value = right_subtree.root.p
+                max_value = right_subtree.p
 
             return Node(left_subtree, right_subtree, splitting_value, max_value)
 
@@ -65,26 +60,26 @@ class Range_tree:
             reported_leaves = []
 
             # Follow the path to x and report the points in subtrees right of the path
-            v = v_split.left_subtree.root
+            v = v_split.left_subtree
             while isinstance(v, Node):
                 if interval[0] <= v.splitting_value:
-                    reported_leaves.extend(self.report_subtree(v.right_subtree.root))
-                    v = v.left_subtree.root
+                    reported_leaves = self.report_subtree(v.right_subtree) + reported_leaves
+                    v = v.left_subtree
                 else:
-                    v = v.right_subtree.root
+                    v = v.right_subtree
             if v.p >= interval[0] and v.p <= interval[1]:
-                reported_leaves.extend(v.report())
+                reported_leaves = v.report() + reported_leaves
 
             # follow the path to x' and report the points in subtrees left of the path
-            v = v_split.right_subtree.root
+            v = v_split.right_subtree
             while isinstance(v, Node):
                 if interval[1] >= v.splitting_value:
-                    reported_leaves.extend(self.report_subtree(v.left_subtree.root))
-                    v = v.right_subtree.root
+                    reported_leaves.extend(self.report_subtree(v.left_subtree))
+                    v = v.right_subtree
                 else:
-                    v = v.left_subtree.root
+                    v = v.left_subtree
             if v.p >= interval[0] and v.p <= interval[1]:
-                reported_leaves.extend(v.report())
+                reported_leaves = v.report() + reported_leaves
 
         return reported_leaves
 
@@ -100,30 +95,30 @@ class Range_tree:
             count = v_split.number_of_points
 
             # Follow the path to x and report the points in subtrees right of the path
-            v = v_split.left_subtree.root
+            v = v_split.left_subtree
             while isinstance(v, Node):
                 if interval[0] <= v.splitting_value:
-                    v = v.left_subtree.root
+                    v = v.left_subtree
                 else:
-                    if isinstance(v.left_subtree.root, Node):
-                        count -= v.left_subtree.root.number_of_points
+                    if isinstance(v.left_subtree, Node):
+                        count -= v.left_subtree.number_of_points
                     else:
                         count -= 1
-                    v = v.right_subtree.root
+                    v = v.right_subtree
             if not (v.p >= interval[0] and v.p <= interval[1]):
                 count -= 1
 
             # follow the path to x' and report the points in subtrees left of the path
-            v = v_split.right_subtree.root
+            v = v_split.right_subtree
             while isinstance(v, Node):
                 if interval[1] >= v.splitting_value:
-                    v = v.right_subtree.root
+                    v = v.right_subtree
                 else:
-                    if isinstance(v.left_subtree.root, Node):
-                        count -= v.right_subtree.root.number_of_points
+                    if isinstance(v.left_subtree, Node):
+                        count -= v.right_subtree.number_of_points
                     else:
                         count -= 1
-                    v = v.left_subtree.root
+                    v = v.left_subtree
             if not( v.p >= interval[0] and v.p <= interval[1]):
                 count -= 1
 
@@ -140,29 +135,29 @@ class Range_tree:
         else:
             # Follow the path to x and report the points in subtrees right of the path
             max_weight_left = v_split.max_weight
-            v = v_split.left_subtree.root
+            v = v_split.left_subtree
             while isinstance(v, Node):
                 if interval[0] <= v.splitting_value:
-                    v = v.left_subtree.root
+                    v = v.left_subtree
                 else:
-                    if isinstance(v.right_subtree.root, Node):
-                        max_weight_left = v.right_subtree.root.max_weight
+                    if isinstance(v.right_subtree, Node):
+                        max_weight_left = v.right_subtree.max_weight
                     else:
-                        max_weight_left = v.right_subtree.root.w
-                    v = v.right_subtree.root
+                        max_weight_left = v.right_subtree.w
+                    v = v.right_subtree
 
             # follow the path to x' and report the points in subtrees left of the path
             max_weight_right = v_split.max_weight
-            v = v_split.right_subtree.root
+            v = v_split.right_subtree
             while isinstance(v, Node):
                 if interval[1] >= v.splitting_value:
-                    v = v.right_subtree.root
+                    v = v.right_subtree
                 else:
-                    if isinstance(v.left_subtree.root, Node):
-                        max_weight_right = v.left_subtree.root.max_weight
+                    if isinstance(v.left_subtree, Node):
+                        max_weight_right = v.left_subtree.max_weight
                     else:
-                        max_weight_right = v.left_subtree.root.w
-                    v = v.left_subtree.root
+                        max_weight_right = v.left_subtree.w
+                    v = v.left_subtree
 
         return max(max_weight_left, max_weight_right)
 
@@ -173,23 +168,23 @@ class Range_tree:
         v = self.root
         while isinstance(v, Node) and (interval[1] <= v.splitting_value or interval[0] > v.splitting_value):
             if interval[1] <= v.splitting_value:
-                v = v.left_subtree.root
+                v = v.left_subtree
             else:
-                v = v.right_subtree.root
+                v = v.right_subtree
         return v
 
 
-    def report_subtree(self, root):
-        if isinstance(root, Leaf):
-            return root.report()
-        return self.report_subtree(root.left_subtree.root) + self.report_subtree(root.right_subtree.root)
+    def report_subtree(self, node):
+        if isinstance(node, Leaf):
+            return node.report()
+        return self.report_subtree(node.left_subtree) + self.report_subtree(node.right_subtree)
 
     def add_properties(self, v):
         if isinstance(v, Leaf):
             return 1, v.w
 
-        left_properties = self.add_properties(v.left_subtree.root)
-        right_properties = self.add_properties(v.right_subtree.root)
+        left_properties = self.add_properties(v.left_subtree)
+        right_properties = self.add_properties(v.right_subtree)
 
         v.number_of_points = left_properties[0] + right_properties[0]
         v.max_weight = max(left_properties[1], right_properties[1])
